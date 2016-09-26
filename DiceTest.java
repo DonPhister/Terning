30e7e82b31c15ff460a804dccec8f3ee02b2f055
mortentang <mortentc@hotmail.com>
2016-09-26 15:24:25 +0200

    Add files via upload

---
+++ DiceTest.java
@@ -0,0 +1,45 @@
+package terning;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiceTest {
+
+	@Test
+	public void test() {
+		Dice terning = new Dice();
+		int et, to, tre, fire, fem, seks, i, m;
+		et=0;
+		to=0;
+		tre=0;
+		fire=0;
+		fem=0;
+		seks=0;
+		m=0;
+		while (m<60000){
+		i=terning.roll();
+		switch (i){
+		case 1: et++; m++;
+		break;
+		case 2:  to++; m++;
+		break;
+		case 3: tre++; m++;
+		break;
+		case 4: fire++; m++;
+		break;
+		case 5: fem++; m++;
+		break;
+		case 6: seks++; m++;
+		}
+		}
+		System.out.print("1'ere = " + et );
+		System.out.print(" , 2'ere = " + to);
+		System.out.print(" , 3'ere = " + tre);
+		System.out.print(" , 4'ere = " + fire);
+		System.out.print(" , 5'ere = " + fem);
+		System.out.println(" , 6'ere = " + seks);
+		System.out.print("Total: " + m);		
+	}
+
+}
